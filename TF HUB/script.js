23c5908baa2e2b2b629d02535f7ce53e04800635
SuporteTiTF <suporteti@bageacademy.com.br>
2023-04-28 17:18:23 -0300

    Update script.js

---
+++ TF HUB/script.js
@@ -0,0 +1,38 @@
+/*variaveis 
+*/
+var a = 2;
+var b = 3;
+var c = "Olá mundo!";
+var soma = a + b;
+var sub = a - b;
+var mult = a * b;
+var div = a / b;
+
+/*alert - função de print
+*/
+alert(soma);
+alert(sub);
+alert(mult);
+alert(div);
+
+/*% - modulo/resto
+
+alert(9%2);*/
+
+/*estrutura de condição
+*/
+if (c % 2 == 1) {
+    alert("Número ímpar");
+}
+else if (c % 2 == 0) {
+    alert("Número Par");
+}
+else {
+    alert("Valor Inválido");
+
+}
+
+
+
+
+
