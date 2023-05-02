7cd353c451e47df0aacf1e2a0b33c8a63f414b8d
SuporteTiTF <suporteti@bageacademy.com.br>
2023-05-02 17:42:15 -0300

    Update script.js

--- TF HUB/script.js
+++ TF HUB/script.js
@@ -32,6 +32,28 @@ else {
 
 }
 
+/*estrutura de repetição
+*/
+var i = 0;
+while(i<3){
+    alert(i);
+    i = i+1;
+}
+
+
+for(var j = 0; j<3; j++){
+    alert (j);
+}
+
+/*tipos de dados
+*/
+
+var numero =6;
+var decimal = 4.5;
+var texto = "Olá Mundo";
+var lista = ["Laranja", "Maça", "Banana"];
+
+
 
 
 
