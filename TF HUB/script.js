8621073017c814fdfca3d14110f3c4a197d1eb20
SuporteTiTF <suporteti@bageacademy.com.br>
2023-05-03 16:22:58 -0300

    Update script.js

--- TF HUB/script.js
+++ TF HUB/script.js
@@ -1,57 +1,18 @@
-/*variaveis 
-*/
-var a = 2;
-var b = 3;
-var c = "Olá mundo!";
-var soma = a + b;
-var sub = a - b;
-var mult = a * b;
-var div = a / b;
-
-/*alert - função de print
-*/
-alert(soma);
-alert(sub);
-alert(mult);
-alert(div);
-
-/*% - modulo/resto
-
-alert(9%2);*/
-
-/*estrutura de condição
-*/
-if (c % 2 == 1) {
-    alert("Número ímpar");
-}
-else if (c % 2 == 0) {
-    alert("Número Par");
-}
-else {
-    alert("Valor Inválido");
-
-}
-
-/*estrutura de repetição
-*/
-var i = 0;
-while(i<3){
-    alert(i);
-    i = i+1;
-}
-
-
-for(var j = 0; j<3; j++){
-    alert (j);
-}
-
-/*tipos de dados
+/*console.log - função de print pelo console do navegador
 */
 
-var numero =6;
-var decimal = 4.5;
-var texto = "Olá Mundo";
-var lista = ["Laranja", "Maça", "Banana"];
+console.log("Olá Mundo")
+
+/*função
+*/
+
+function soma(a,b){
+    console.log(a+b);
+}
+
+soma (2,2)
+
+
 
 
 
