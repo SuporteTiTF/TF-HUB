c3a2a98caecf0a43f8720fb714652bdca9f404c3
SuporteTiTF <suporteti@bageacademy.com.br>
2023-05-04 17:30:58 -0300

    Update script.js

--- TF HUB/script.js
+++ TF HUB/script.js
@@ -1,44 +1,18 @@
-/*console.log - função de print pelo console do navegador
-*/
+function nomevalida(){
+	var nome = document.getElementById('nome');
+    var email = document.getElementById('email');
+    var mensagem = document.getElementById('mensagem');
 
-console.log("Olá Mundo")
-
-/*função
-*/
-
-function soma(a,b){
-    console.log(a+b);
+	if (nome.value == ""){
+		alert("O campo nome não pode estar em branco.");
+	}
+    else if (email.value == ""){
+		alert("O campo email não pode estar em branco.");
+	}
+    else if (mensagem.value == ""){
+		alert("O campo mensagem não pode estar em branco.");
+	}
+	else {
+		alert("Nenhum problema foi detectado. Seu formulário pode ser enviado com sucesso.");
+	}
 }
-
-soma (2,2)
-
-function subtracao(a,b){
-    return a-b;
-}
-var s = subtracao(5,3);
-console.log(s);
-
-function multiplicacao(a,b){
-    return a*b;
-}
-console.log(multiplicacao(5,5));
-
-/*eventos 
-*/
-//<button onclick="mensagem()">Clique Aqui</button>
-
-function mensagem(){
-    alert("Não clique em mim");
-}
-
-
-
-
-
-
-
-
-
-
-
-
