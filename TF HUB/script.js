73bb349bb972a372a63a2926ec940cc64fa972df
SuporteTiTF <suporteti@bageacademy.com.br>
2023-05-08 17:26:51 -0300

    Update script.js

--- TF HUB/script.js
+++ TF HUB/script.js
@@ -17,19 +17,23 @@ function nomevalida(){
 	}
 }
 
-//https://www.youtube.com/watch?v=2umsItNQ9mI 
-//https://www.amazon.com.br/exec/obidos/ASIN/B094YR4SLJ?linkId=f9f6d7870afc9a4982e100a0120bbbe1&language=pt_BR&ref_=as_li_ss_tl&th=1
-//https://www.amazon.com.br/gp/product/B078K1CHB5?smid=A3PTXMYVQ9LX5U&psc=1&linkCode=sl1&tag=peguepromo01f-20&linkId=94053c684df7654390324fdbd87a8ffe&language=pt_BR&ref_=as_li_ss_tl
-//https://produto.mercadolivre.com.br/MLB-2745842194-carregador-turbo-20w-baseus-para-iphone-13-12-11-cabo-1m-_JM?matt_tool=82123253&matt_word=gesiel_telegram_gtAfiliados&forceInApp=true
-//https://www.banggood.com/Baseus-30000mAh-111Wh-20W-PD-Power-Bank-External-Battery-Power-Supply-With-20W-USB-C-PD-and-18W-USB-A+2-QC3_0-Output-FCP-AFC-Fast-Charging-For-iPhone-13-Pro-Max-For-Samsung-Galaxy-S21-5G-p-1948181.html?p=1I06039310387201703H&cur_warehouse=CN
-//https://produto.mercadolivre.com.br/MLB-3259598203-carregador-carga-rapida-power-bank-30000mah-turbo-portatil-_JM?matt_tool=15674050&matt_word=&matt_source=google&matt_campaign_id=14303413808&matt_ad_group_id=125984298237&matt_match_type=&matt_network=g&matt_device=c&matt_creative=539354956962&matt_keyword=&matt_ad_position=&matt_ad_type=pla&matt_merchant_id=565141152&matt_product_id=MLB3259598203&matt_product_partition_id=1818294903520&matt_target_id=pla-1818294903520
-//https://www.magazineluiza.com.br/carregbateria-portatil-power-bank-30-000mah-carrega-celular-notbook-tablet-de-6-a-12-cargas-h-hm/p/ahf9kc863h/te/accp/?&seller_id=galocellmonteiro&utm_campaign=&partner_id=69106
-//https://shopee.com.br/Carregador-Port%C3%A1til-De-Celular-Bateria-Externa-Original-Power-Bank-Pineng-20000mah-At%C3%A9-10-Cargas-Universal-i.741746705.17943173515?sp_atk=dcfae639-d489-4e7e-8f95-0caabb72b933&xptdk=dcfae639-d489-4e7e-8f95-0caabb72b933
-//https://shopee.com.br/Carregador-Power-Bank-Xiaomi-30000mAh-Fast-Charging-18W-Orignal-i.824975864.22817675353?sp_atk=6292ad05-8b42-45d6-8854-85c9b5602f5a&xptdk=6292ad05-8b42-45d6-8854-85c9b5602f5a
-//https://shopee.com.br/Carregador-Port%C3%A1til-Power-Bank-30.000mah-Super-Carga-R%C3%A1pida-18W-Hmaston-i.376513922.21027949455?sp_atk=b13d1c50-c1e3-4387-b995-f16070583bc9&xptdk=b13d1c50-c1e3-4387-b995-f16070583bc9
-//https://shopee.com.br/Carregador-Port%C3%A1til-Turbo-Power-Bank-20000mah-Original-i.410522123.18430826357?sp_atk=d9e7bdfe-c9b6-4137-b421-e35dd192793e&xptdk=d9e7bdfe-c9b6-4137-b421-e35dd192793e
-//https://shopee.com.br/Carregador-Port%C3%A1til-Power-Bank-R%C3%A1pido-20000mah-2-Sa%C3%ADdas-i.410522123.19831543849?sp_atk=9b6b8f8a-1837-4671-a5ad-b04184dfb942&xptdk=9b6b8f8a-1837-4671-a5ad-b04184dfb942
-//https://shopee.com.br/Carregador-Power-Bank-Xiaomi-30000mAh-Fast-Charging-18W-Orignal-i.824975864.22817675353?sp_atk=6292ad05-8b42-45d6-8854-85c9b5602f5a&xptdk=6292ad05-8b42-45d6-8854-85c9b5602f5a
+//https://www.youtube.com/watch?v=2umsItNQ9mI
+//https://formsubmit.co/
+
+// 20.000
+//108,81 - https://shopee.com.br/Carregador-Port%C3%A1til-Power-Bank-R%C3%A1pido-20000mah-2-Sa%C3%ADdas-i.410522123.19831543849?sp_atk=9b6b8f8a-1837-4671-a5ad-b04184dfb942&xptdk=9b6b8f8a-1837-4671-a5ad-b04184dfb942
+//129,99 - https://shopee.com.br/Carregador-Port%C3%A1til-De-Celular-Bateria-Externa-Original-Power-Bank-Pineng-20000mah-At%C3%A9-10-Cargas-Universal-i.741746705.17943173515?sp_atk=dcfae639-d489-4e7e-8f95-0caabb72b933&xptdk=dcfae639-d489-4e7e-8f95-0caabb72b933
+//136,77 - https://shopee.com.br/Carregador-Port%C3%A1til-Turbo-Power-Bank-20000mah-Original-i.410522123.18430826357?sp_atk=d9e7bdfe-c9b6-4137-b421-e35dd192793e&xptdk=d9e7bdfe-c9b6-4137-b421-e35dd192793e
+//156,50 - https://www.amazon.com.br/gp/product/B078K1CHB5?smid=A3PTXMYVQ9LX5U&psc=1&linkCode=sl1&tag=peguepromo01f-20&linkId=94053c684df7654390324fdbd87a8ffe&language=pt_BR&ref_=as_li_ss_tl
+
+//229,90 - https://www.amazon.com.br/exec/obidos/ASIN/B094YR4SLJ?linkId=f9f6d7870afc9a4982e100a0120bbbe1&language=pt_BR&ref_=as_li_ss_tl&th=1
+//30.000
+//182,60 - https://shopee.com.br/Carregador-Port%C3%A1til-Power-Bank-30.000mah-Super-Carga-R%C3%A1pida-18W-Hmaston-i.376513922.21027949455?sp_atk=b13d1c50-c1e3-4387-b995-f16070583bc9&xptdk=b13d1c50-c1e3-4387-b995-f16070583bc9
+//197,50 - https://shopee.com.br/Carregador-Power-Bank-Xiaomi-30000mAh-Fast-Charging-18W-Orignal-i.824975864.22817675353?sp_atk=6292ad05-8b42-45d6-8854-85c9b5602f5a&xptdk=6292ad05-8b42-45d6-8854-85c9b5602f5a
+
+//197,50 - https://shopee.com.br/Carregador-Power-Bank-Xiaomi-30000mAh-Fast-Charging-18W-Orignal-i.824975864.22817675353?sp_atk=6292ad05-8b42-45d6-8854-85c9b5602f5a&xptdk=6292ad05-8b42-45d6-8854-85c9b5602f5a
+//203,56 - https://www.banggood.com/Baseus-30000mAh-111Wh-20W-PD-Power-Bank-External-Battery-Power-Supply-With-20W-USB-C-PD-and-18W-USB-A+2-QC3_0-Output-FCP-AFC-Fast-Charging-For-iPhone-13-Pro-Max-For-Samsung-Galaxy-S21-5G-p-1948181.html?p=1I06039310387201703H&cur_warehouse=CN
+//219 - https://www.magazineluiza.com.br/carregbateria-portatil-power-bank-30-000mah-carrega-celular-notbook-tablet-de-6-a-12-cargas-h-hm/p/ahf9kc863h/te/accp/?&seller_id=galocellmonteiro&utm_campaign=&partner_id=69106
 //https://shopee.com.br/Carregador-Port%C3%A1til-Power-Bank-30.000mah-Super-Carga-R%C3%A1pida-18W-Hmaston-i.376513922.21027949455?sp_atk=b13d1c50-c1e3-4387-b995-f16070583bc9&xptdk=b13d1c50-c1e3-4387-b995-f16070583bc9
 //https://shopee.com.br/Carregador-Port%C3%A1til-Power-Bank-30000mah-H-maston-Original-i.231065623.19622916901?sp_atk=de3ec920-5d9e-4cd7-be3d-8604bdaeafdb&xptdk=de3ec920-5d9e-4cd7-be3d-8604bdaeafdb
 //https://shopee.com.br/Xiaomi-Power-bank-30000mAh-Bateria-Port%C3%A1til-Power-Bank-Xiaomi-Original-entrada-Type-C-e-Micro-USB-i.932797104.23519741333?sp_atk=18b01859-3c57-4139-bfc6-683acaf493dc&xptdk=18b01859-3c57-4139-bfc6-683acaf493dc
@@ -45,6 +49,7 @@ function nomevalida(){
 //https://shopee.com.br/Power-Bank-Original-30000mah-12-Carregador-Port%C3%A1til-Cargas-Extra-para-Celular-i.275209344.21961298448?sp_atk=f3dcb52a-20a8-47e8-9d1c-d233b74331d2&xptdk=f3dcb52a-20a8-47e8-9d1c-d233b74331d2
 //https://shopee.com.br/Carregador-Port%C3%A1til-30000mah-Comp%C3%A1tivel-Samsung-iPhone-iPad-i.378612120.17887520864?sp_atk=06df6e22-22ca-46a8-a1c6-e7a39f40dc6c&xptdk=06df6e22-22ca-46a8-a1c6-e7a39f40dc6c
 //https://shopee.com.br/Carregador-Port%C3%A1til-Power-Bank-Turbo-30000mah-Carga-R%C3%A1pida.-ORIGINAL-i.377169476.15278985857?sp_atk=450e9efb-2b81-46cc-a62c-0535e826deb9&xptdk=450e9efb-2b81-46cc-a62c-0535e826deb9
-//https://formsubmit.co/
 //https://shopee.com.br/Carregador-Port%C3%A1til-Power-Bank-Turbo-Pro-30000-Mah-Original-i.391049437.18172070959?sp_atk=3a3e2f82-9fd3-4023-bae0-db14488221de&xptdk=3a3e2f82-9fd3-4023-bae0-db14488221de
 //https://shopee.com.br/Carregador-Port%C3%A1til-Power-Bank-Turbo-30000mah-Carga-R%C3%A1pida.-ORIGINAL-i.377169476.15278985857?sp_atk=450e9efb-2b81-46cc-a62c-0535e826deb9&xptdk=450e9efb-2b81-46cc-a62c-0535e826deb9
+//Carregador
+//https://produto.mercadolivre.com.br/MLB-2745842194-carregador-turbo-20w-baseus-para-iphone-13-12-11-cabo-1m-_JM?matt_tool=82123253&matt_word=gesiel_telegram_gtAfiliados&forceInApp=true
