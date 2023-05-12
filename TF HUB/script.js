c9d00c2edb8cf90d7bda4579cf713b61f44d7fa4
SuporteTiTF <suporteti@bageacademy.com.br>
2023-05-12 17:21:46 -0300

    Update script.js

--- TF HUB/script.js
+++ TF HUB/script.js
@@ -32,6 +32,7 @@ function nomevalida(){
 //https://www.youtube.com/watch?v=SQR7dlTDauc
 //https://www.youtube.com/watch?v=TrfhQwSYCEk
 // parei nesse - https://www.youtube.com/watch?v=GS5Q_XxsIR4&list=PLOUrDmh7c7mVzTETBVBerrMawaLd-4RMs&index=7
+
 //paletas
 //#BCCFDE
 //#476C9B
@@ -49,31 +50,31 @@ function nomevalida(){
 //129,99 - https://shopee.com.br/Carregador-Port%C3%A1til-De-Celular-Bateria-Externa-Original-Power-Bank-Pineng-20000mah-At%C3%A9-10-Cargas-Universal-i.741746705.17943173515?sp_atk=dcfae639-d489-4e7e-8f95-0caabb72b933&xptdk=dcfae639-d489-4e7e-8f95-0caabb72b933
 //136,77 - https://shopee.com.br/Carregador-Port%C3%A1til-Turbo-Power-Bank-20000mah-Original-i.410522123.18430826357?sp_atk=d9e7bdfe-c9b6-4137-b421-e35dd192793e&xptdk=d9e7bdfe-c9b6-4137-b421-e35dd192793e
 //156,50 - https://www.amazon.com.br/gp/product/B078K1CHB5?smid=A3PTXMYVQ9LX5U&psc=1&linkCode=sl1&tag=peguepromo01f-20&linkId=94053c684df7654390324fdbd87a8ffe&language=pt_BR&ref_=as_li_ss_tl
-
 //229,90 - https://www.amazon.com.br/exec/obidos/ASIN/B094YR4SLJ?linkId=f9f6d7870afc9a4982e100a0120bbbe1&language=pt_BR&ref_=as_li_ss_tl&th=1
+
 //30.000
+//179 - https://shopee.com.br/Carregador-port%C3%A1til-Xiaomi-powerbank-30000mah-fast-charger-i.439648607.18176713105?sp_atk=16a2c8fc-9438-49dd-b577-90cfa17b9c90&xptdk=16a2c8fc-9438-49dd-b577-90cfa17b9c90
+//182,60 - https://shopee.com.br/Carregador-Port%C3%A1til-Power-Bank-30.000mah-Super-Carga-R%C3%A1pida-18W-Hmaston-i.376513922.21027949455?sp_atk=b13d1c50-c1e3-4387-b995-f16070583bc9&xptdk=b13d1c50-c1e3-4387-b995-f16070583bc9
 //182,60 - https://shopee.com.br/Carregador-Port%C3%A1til-Power-Bank-30.000mah-Super-Carga-R%C3%A1pida-18W-Hmaston-i.376513922.21027949455?sp_atk=b13d1c50-c1e3-4387-b995-f16070583bc9&xptdk=b13d1c50-c1e3-4387-b995-f16070583bc9
 //197,50 - https://shopee.com.br/Carregador-Power-Bank-Xiaomi-30000mAh-Fast-Charging-18W-Orignal-i.824975864.22817675353?sp_atk=6292ad05-8b42-45d6-8854-85c9b5602f5a&xptdk=6292ad05-8b42-45d6-8854-85c9b5602f5a
-
 //197,50 - https://shopee.com.br/Carregador-Power-Bank-Xiaomi-30000mAh-Fast-Charging-18W-Orignal-i.824975864.22817675353?sp_atk=6292ad05-8b42-45d6-8854-85c9b5602f5a&xptdk=6292ad05-8b42-45d6-8854-85c9b5602f5a
+//199 - https://shopee.com.br/Carregador-Port%C3%A1til-Power-Bank-Pineng-Pn-899pd-30000-Mah-i.553049500.22415245341?sp_atk=70f4cc8b-55c4-4d4f-985e-37a8673dbd92&xptdk=70f4cc8b-55c4-4d4f-985e-37a8673dbd92
+//200 - https://shopee.com.br/Carregador-Port%C3%A1til-Power-Bank-Turbo-Pro-30000-Mah-Original-i.391049437.18172070959?sp_atk=3a3e2f82-9fd3-4023-bae0-db14488221de&xptdk=3a3e2f82-9fd3-4023-bae0-db14488221de
 //203,56 - https://www.banggood.com/Baseus-30000mAh-111Wh-20W-PD-Power-Bank-External-Battery-Power-Supply-With-20W-USB-C-PD-and-18W-USB-A+2-QC3_0-Output-FCP-AFC-Fast-Charging-For-iPhone-13-Pro-Max-For-Samsung-Galaxy-S21-5G-p-1948181.html?p=1I06039310387201703H&cur_warehouse=CN
+//204 - https://shopee.com.br/Carregador-Portatil-Power-Bank-30000mah-Original-Universal-i.491695317.22934812628?sp_atk=d42ba6c2-b97f-4d9e-9a88-39bf15b2ee33&xptdk=d42ba6c2-b97f-4d9e-9a88-39bf15b2ee33
+//210 - https://shopee.com.br/Carregador-port%C3%A1til-Power-Bank-TURBO-30000-MAH-tipo-c-e-V8-com-visor-de-led-i.303206341.15021859150?sp_atk=1c634027-95cf-427d-bbc0-9e332997293c&xptdk=1c634027-95cf-427d-bbc0-9e332997293c
+//213 - https://shopee.com.br/PowerBank-Carregador-Bateria-Portatil-30000mAh-Carga-R%C3%A1pida-Original-USB-TipoC-Micro-USB-i.462556384.12496872943?sp_atk=8807c2e2-ef7a-42e7-9d9d-28a5bfceb48e&xptdk=8807c2e2-ef7a-42e7-9d9d-28a5bfceb48e
 //219 - https://www.magazineluiza.com.br/carregbateria-portatil-power-bank-30-000mah-carrega-celular-notbook-tablet-de-6-a-12-cargas-h-hm/p/ahf9kc863h/te/accp/?&seller_id=galocellmonteiro&utm_campaign=&partner_id=69106
-//https://shopee.com.br/Carregador-Port%C3%A1til-Power-Bank-30.000mah-Super-Carga-R%C3%A1pida-18W-Hmaston-i.376513922.21027949455?sp_atk=b13d1c50-c1e3-4387-b995-f16070583bc9&xptdk=b13d1c50-c1e3-4387-b995-f16070583bc9
-//https://shopee.com.br/Carregador-Port%C3%A1til-Power-Bank-30000mah-H-maston-Original-i.231065623.19622916901?sp_atk=de3ec920-5d9e-4cd7-be3d-8604bdaeafdb&xptdk=de3ec920-5d9e-4cd7-be3d-8604bdaeafdb
-//https://shopee.com.br/Xiaomi-Power-bank-30000mAh-Bateria-Port%C3%A1til-Power-Bank-Xiaomi-Original-entrada-Type-C-e-Micro-USB-i.932797104.23519741333?sp_atk=18b01859-3c57-4139-bfc6-683acaf493dc&xptdk=18b01859-3c57-4139-bfc6-683acaf493dc
-//https://shopee.com.br/Carregador-Port%C3%A1til-Turbo-Power-Bank-30000mah-H-maston-100-Original-(Preto-E-Branco)-i.854590033.23613207613?sp_atk=3dfb62b8-8caa-4275-be15-5178ee143bad&xptdk=3dfb62b8-8caa-4275-be15-5178ee143bad
-//https://shopee.com.br/Carregador-Port%C3%A1til-Power-Bank-Pineng-Pn-899pd-30000-Mah-i.553049500.22415245341?sp_atk=70f4cc8b-55c4-4d4f-985e-37a8673dbd92&xptdk=70f4cc8b-55c4-4d4f-985e-37a8673dbd92
-//https://shopee.com.br/PowerBank-Carregador-Bateria-Portatil-30000mAh-Carga-R%C3%A1pida-Original-USB-TipoC-Micro-USB-i.462556384.12496872943?sp_atk=8807c2e2-ef7a-42e7-9d9d-28a5bfceb48e&xptdk=8807c2e2-ef7a-42e7-9d9d-28a5bfceb48e
-//https://shopee.com.br/Carregador-port%C3%A1til-Xiaomi-powerbank-30000mah-fast-charger-i.439648607.18176713105?sp_atk=16a2c8fc-9438-49dd-b577-90cfa17b9c90&xptdk=16a2c8fc-9438-49dd-b577-90cfa17b9c90
-//https://shopee.com.br/Carregador-Port%C3%A1til-Power-Bank-Turbo-30000mah.-Original-i.377169476.14052687714?sp_atk=ad5efa72-4990-4891-915c-55d9fe5d5838&xptdk=ad5efa72-4990-4891-915c-55d9fe5d5838
-//https://shopee.com.br/Carregador-Port%C3%A1til-R%C3%A1pido-(15W)-Mini-Ja-Baseus-30000mAh-i.763975215.19752813779?sp_atk=db56e996-48b2-4946-a529-5d4a0b6ee95a&xptdk=db56e996-48b2-4946-a529-5d4a0b6ee95a
-//https://shopee.com.br/Carregador-port%C3%A1til-Power-Bank-TURBO-30000-MAH-tipo-c-e-V8-com-visor-de-led-i.303206341.15021859150?sp_atk=1c634027-95cf-427d-bbc0-9e332997293c&xptdk=1c634027-95cf-427d-bbc0-9e332997293c
-//https://shopee.com.br/Carregador-Port%C3%A1til-Power-Bank-30000mah-Super-Carga-R%C3%A1pida-18W-Hmaston-i.224160635.16983309827?sp_atk=57830a7c-9334-4bc6-8a11-477fa9b97961&xptdk=57830a7c-9334-4bc6-8a11-477fa9b97961
-//https://shopee.com.br/Carregador-Portatil-Power-Bank-30000mah-Original-Universal-i.491695317.22934812628?sp_atk=d42ba6c2-b97f-4d9e-9a88-39bf15b2ee33&xptdk=d42ba6c2-b97f-4d9e-9a88-39bf15b2ee33
-//https://shopee.com.br/Power-Bank-Original-30000mah-12-Carregador-Port%C3%A1til-Cargas-Extra-para-Celular-i.275209344.21961298448?sp_atk=f3dcb52a-20a8-47e8-9d1c-d233b74331d2&xptdk=f3dcb52a-20a8-47e8-9d1c-d233b74331d2
-//https://shopee.com.br/Carregador-Port%C3%A1til-30000mah-Comp%C3%A1tivel-Samsung-iPhone-iPad-i.378612120.17887520864?sp_atk=06df6e22-22ca-46a8-a1c6-e7a39f40dc6c&xptdk=06df6e22-22ca-46a8-a1c6-e7a39f40dc6c
-//https://shopee.com.br/Carregador-Port%C3%A1til-Power-Bank-Turbo-30000mah-Carga-R%C3%A1pida.-ORIGINAL-i.377169476.15278985857?sp_atk=450e9efb-2b81-46cc-a62c-0535e826deb9&xptdk=450e9efb-2b81-46cc-a62c-0535e826deb9
-//https://shopee.com.br/Carregador-Port%C3%A1til-Power-Bank-Turbo-Pro-30000-Mah-Original-i.391049437.18172070959?sp_atk=3a3e2f82-9fd3-4023-bae0-db14488221de&xptdk=3a3e2f82-9fd3-4023-bae0-db14488221de
-//https://shopee.com.br/Carregador-Port%C3%A1til-Power-Bank-Turbo-30000mah-Carga-R%C3%A1pida.-ORIGINAL-i.377169476.15278985857?sp_atk=450e9efb-2b81-46cc-a62c-0535e826deb9&xptdk=450e9efb-2b81-46cc-a62c-0535e826deb9
+//229 - https://shopee.com.br/Carregador-Port%C3%A1til-30000mah-Comp%C3%A1tivel-Samsung-iPhone-iPad-i.378612120.17887520864?sp_atk=06df6e22-22ca-46a8-a1c6-e7a39f40dc6c&xptdk=06df6e22-22ca-46a8-a1c6-e7a39f40dc6c
+//229 - https://shopee.com.br/Carregador-Port%C3%A1til-Power-Bank-Turbo-30000mah-Carga-R%C3%A1pida.-ORIGINAL-i.377169476.15278985857?sp_atk=450e9efb-2b81-46cc-a62c-0535e826deb9&xptdk=450e9efb-2b81-46cc-a62c-0535e826deb9
+//229 - https://shopee.com.br/Carregador-Port%C3%A1til-Power-Bank-Turbo-30000mah-Carga-R%C3%A1pida.-ORIGINAL-i.377169476.15278985857?sp_atk=450e9efb-2b81-46cc-a62c-0535e826deb9&xptdk=450e9efb-2b81-46cc-a62c-0535e826deb9
+//229 - https://shopee.com.br/Carregador-Port%C3%A1til-Turbo-Power-Bank-30000mah-H-maston-100-Original-(Preto-E-Branco)-i.854590033.23613207613?sp_atk=3dfb62b8-8caa-4275-be15-5178ee143bad&xptdk=3dfb62b8-8caa-4275-be15-5178ee143bad
+//230 - https://shopee.com.br/Carregador-Port%C3%A1til-Power-Bank-Turbo-30000mah.-Original-i.377169476.14052687714?sp_atk=ad5efa72-4990-4891-915c-55d9fe5d5838&xptdk=ad5efa72-4990-4891-915c-55d9fe5d5838
+//230 - https://shopee.com.br/Carregador-Port%C3%A1til-Power-Bank-30000mah-Super-Carga-R%C3%A1pida-18W-Hmaston-i.224160635.16983309827?sp_atk=57830a7c-9334-4bc6-8a11-477fa9b97961&xptdk=57830a7c-9334-4bc6-8a11-477fa9b97961
+//239 - https://shopee.com.br/Carregador-Port%C3%A1til-Power-Bank-30000mah-H-maston-Original-i.231065623.19622916901?sp_atk=de3ec920-5d9e-4cd7-be3d-8604bdaeafdb&xptdk=de3ec920-5d9e-4cd7-be3d-8604bdaeafdb
+//249 - https://shopee.com.br/Power-Bank-Original-30000mah-12-Carregador-Port%C3%A1til-Cargas-Extra-para-Celular-i.275209344.21961298448?sp_atk=f3dcb52a-20a8-47e8-9d1c-d233b74331d2&xptdk=f3dcb52a-20a8-47e8-9d1c-d233b74331d2
+//286 - https://shopee.com.br/Xiaomi-Power-bank-30000mAh-Bateria-Port%C3%A1til-Power-Bank-Xiaomi-Original-entrada-Type-C-e-Micro-USB-i.932797104.23519741333?sp_atk=18b01859-3c57-4139-bfc6-683acaf493dc&xptdk=18b01859-3c57-4139-bfc6-683acaf493dc
+//299 - https://shopee.com.br/Carregador-Port%C3%A1til-R%C3%A1pido-(15W)-Mini-Ja-Baseus-30000mAh-i.763975215.19752813779?sp_atk=db56e996-48b2-4946-a529-5d4a0b6ee95a&xptdk=db56e996-48b2-4946-a529-5d4a0b6ee95a
+
 //Carregador
 //https://produto.mercadolivre.com.br/MLB-2745842194-carregador-turbo-20w-baseus-para-iphone-13-12-11-cabo-1m-_JM?matt_tool=82123253&matt_word=gesiel_telegram_gtAfiliados&forceInApp=true
